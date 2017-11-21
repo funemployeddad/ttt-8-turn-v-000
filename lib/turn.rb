a624101a5d3112e68e25eb58ddb554cdf015d6aa
Josh Kramer <funemployeddad@gmail.com>
2017-11-21 16:20:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,24 @@
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+def move(board, position, char = "X")
+  board[position] = char
 end
 
 def valid_move?(board, index)
 if index.between?(0, 8) && position_taken?(board, index) == false
 true
 end
-end
-
-def move
 end
 
 def position_taken?(board, index)
